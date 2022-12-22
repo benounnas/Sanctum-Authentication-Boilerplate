5f1fd515523a29c54437d819d432087d7ec2de0c
Benounnas Oussama <benounnas.oussama@gmail.com>
2022-12-22 16:43:23 +0100

    cleaning & importing Quasar to laravel

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: { transformAssetUrls },
+        }),
         laravel({
             input: ["resources/css/app.css", "resources/ts/src/main.ts"],
             refresh: true,
+        }),
+        quasar({
+            autoImportComponentCase: "kebab",
+            sassVariables: "./resources/ts/src/quasar-variables.sass",
         }),
     ],
     resolve: {
